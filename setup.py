6cf941cc33dd67ff1d59ebec44c0644249a51bc7
Yufei Pan <pan@zopyr.us>
2025-02-07 02:50:46 -0800

    Add support for MacOS and Windows in setup.py

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
     ],
     python_requires='>=3.6',
 	license='GPLv3+',
